4248310b61881f2227a70d2083d0a0cc9420e561
KAGUMYA AYEBARE <ayebarekagumya177@gmail.com>
2026-06-10 10:40:38 +0300

    Update crud.php

--- crud.php
+++ crud.php
@@ -2,17 +2,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// define("DB_HOST", "localhost");
-// define("USERNAME", "phpuser");
-// define("PASSWORD", "1234");
-// define("DB_NAME", "crud_operations");
+$localhost = "Localhost";
+$username = "DB username";
+$password = "DB password";
+$dbname = "DB NAME";
 
-$localhost = "127.0.0.1";
-$username = "phpuser";
-$password = "1234";
-$dbname = "crud_operations";
-
-// $connection = mysqli_connect("localhost", "phpuser", "1234", "crud_operations");
 $connection = mysqli_connect($localhost, $username, $password, $dbname);
 
 // if (!$connection) {
